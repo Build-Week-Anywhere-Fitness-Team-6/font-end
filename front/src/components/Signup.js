19229aa7b029b9f86c7d5c3405c02aad83d6836a
Allison Homem <mrs.allisonhomem@outlook.com>
2021-09-20 15:16:38 -0500

    some form set-up and some css

--- front/src/components/Signup.js
+++ front/src/components/Signup.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Signup(props) {
+function Signup() {
+    const [newUser, setNewUser] = useState({
+        name: '',
+        username: '',
+        password: '',
+        instructor: false,
+        client: false
+    })
+
+    const handleChange = (event) => {
+        setNewUser({
+            ...newUser,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const submitNewUser = (event) => {
+        event.preventDefault();
+
+
+    }
+
     return (
-        <div>
-            
+        <div className='bg-gym bg-cover bg-opacity-10 h-screen flex flex-col justify-center'>
+          <form className='flex flex-col h-3/5 w-2/5 justify-around align-center self-center bg-gray-50 bg-opacity-60'
+                onSubmit={submitNewUser}>
+            <label>Name:
+              <input type='text' 
+                     placeholder=' full name'
+                     name='name'
+                     value={newUser.name}
+                     onChange={handleChange} />
+            </label>
+
+            <label>Username:
+              <input type='text'
+                     placeholder=' desired username'
+                     name='username'
+                     value={newUser.username}
+                     onChange={handleChange} />
+            </label>
+
+            <label>Password:
+              <input type='text'
+                     placeholder=' desired password'
+                     name='password' 
+                     value={newUser.password}
+                     onChange={handleChange}/>
+            </label>
+
+            <div className='flex justify-evenly'>
+              <label>Instructor:
+                <input type='radio'
+                       name='instructor'
+                       value={newUser.instructor}
+                       onChange={handleChange}/>
+              </label>
+
+              <label>Client:
+                <input type='radio'
+                       name='client'
+                       value={newUser.client}
+                       onChange={handleChange}/>
+              </label>
+            </div>
+
+            <button className="w-3/5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full self-center"
+                    type='submit'>Register</button>
+          </form>
         </div>
     );
 }
