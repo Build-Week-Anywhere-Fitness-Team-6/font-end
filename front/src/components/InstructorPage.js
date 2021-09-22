18869d27187437426132837a786690a1125a00b1
jaredhall18 <jaredhall18@gmail.com>
2021-09-22 10:04:19 -0600

    duplicated Client Page and classList over to Instructor Page and Instructor ClassList

--- front/src/components/InstructorPage.js
+++ front/src/components/InstructorPage.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import InstructorClassList from "./InstructorClassList"
 
-function InstructorPage(props) {
+const dummyData = [
+    {
+    id: 1,
+    name: 'Jareds class', 
+    type: 'Boxing', 
+    time: '12:30 am', 
+    day: 'Thursday', 
+    duration: '30 mins', 
+    intensity: 'Intermediate', 
+    location: 'Seattle, WA', 
+    max_capacity: '50'
+    },
+    {
+        id: 2,
+        name: 'Rocking KickBoxing', 
+        type: 'Boxing', 
+        time: '12:30 am', 
+        day: 'Thursday', 
+        duration: '30 mins', 
+        intensity: 'Intermediate', 
+        location: 'Seattle, WA', 
+        max_capacity: '50'
+        }
+]
+
+
+
+function InstructorPage() {
     return (
-        <div>
-            <h1>Test</h1>
+        <div className="bg-purple-400">
+            <h1 className= "text-gray-50 font-bold text-xl p-3" >All Available Classes</h1>
+
+            {
+                dummyData.map(classes => {
+                return(
+                    <InstructorClassList key={classes.id} details={classes} />
+                )  
+                })
+                
+            }
+            
         </div>
-    );
+    )
 }
 
 export default InstructorPage;
